2dac742e2643f928b2ced7e7dcbd2e6e7ec9ca82
DiegoM <medeldiego47@yahoo.com>
2023-01-19 20:14:11 -0600

    checkout page and router functionality

--- shoemarketplace/src/App.jsx
+++ shoemarketplace/src/App.jsx
@@ -1,11 +1,26 @@
 import React from 'react';
 import { Typography } from '@mui/material';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Checkout from './components/Checkout';
+
 
 const App = () => {
-    return (
+    return (<Router>
+    
         <div>
             <h1>Shoe Marketplace</h1>
+        <Routes>
+      
+        
+            <Route 
+            path='/checkout'
+            element ={<Checkout/>}
+            />
+        
+        </Routes>
+        
         </div>
+        </Router>
     );
 }
 
