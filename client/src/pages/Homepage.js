e6de1b399181f537ad989128413f2ba9ba9ee2a4
nitrammm <martinhuertajr@gmail.com>
2023-01-24 20:07:11 -0600

    added homepage, loginPage, Nopage, signup js

---
+++ client/src/pages/Homepage.js
@@ -0,0 +1,15 @@
+import React from "react";
+import ProductList from "../components/ProductList";
+import Cart from "../components/Cart";
+
+const Home = () => {
+  return (
+
+    <div className="container">
+      <ProductList />
+      <Cart />
+    </div>
+  );
+};
+
+export default Home;
---
+++ client/src/pages/NoPage.js
@@ -0,0 +1,15 @@
+import React from "react";
+import Message from "../components/Message";
+
+const NoPage = () => {
+  return (
+    <div>
+      <Message>
+        <h1>404 Page Not Found</h1>
+  
+      </Message>
+    </div>
+  );
+};
+
+export default NoPage;
